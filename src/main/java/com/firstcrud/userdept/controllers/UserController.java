34b55ff1efaff8c0eb72c5a44c600c221ad94916
Caio Henrique <crobatmobile@gmail.com>
2024-01-14 14:38:36 -0300

    put & delete adicionados na API

--- src/main/java/com/firstcrud/userdept/controllers/UserController.java
+++ src/main/java/com/firstcrud/userdept/controllers/UserController.java
@@ -3,11 +3,16 @@ package com.firstcrud.userdept.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.firstcrud.userdept.UserRepository;
 import com.firstcrud.userdept.entities.User;
@@ -32,8 +37,28 @@ public class UserController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public User insert(@RequestBody User user) {
         User result = repository.save(user);
         return result;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        if(!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        user.setId(id);
+        user = repository.save(user);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        if(!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
